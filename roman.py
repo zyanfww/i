0da77d099199031396739e9b596cb8911c90b23f
zyanfww <3blue1brownhindi@gmail.com>
2026-08-20 05:39:56 +0000

    f

--- roman.py
+++ roman.py
@@ -65,8 +65,8 @@ class Roman(InteractiveScene):
         xx.set_color(GOLD)
 
         self.play(
-            ReplacementTransform(roman["X"][0], xx[0]),
-            ReplacementTransform(roman["X"][1], xx[1]),
+            ReplacementTransform(roman["X"][0][0], xx[0][0]),
+            ReplacementTransform(roman["X"][1][0], xx[1][0]),
         )
         self.play(
             ReplacementTransform(xx, math["20"])
