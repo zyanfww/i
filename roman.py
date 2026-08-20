1c0a1a169cf3fbd0fc5d273973edb0b1436257b6
zyanfww <3blue1brownhindi@gmail.com>
2026-08-20 04:59:00 +0000

    f

--- roman.py
+++ roman.py
@@ -2,7 +2,24 @@ from manimlib import *
 
 class Roman(InteractiveScene):
     def construct(self):
-        t = TexText("A")
-        t2 = TexText("B")
-        self.play(Transform(t, t2))
+        color = [PURPLE_A, PURPLE_C]
+        math = Tex(" 19 - 1 = 20")
+        math.set_color(color)
+        math.shift(2 * UP)
+
+        self.play(Write(math[:5]))
+        self.wait()
+
+        roman = Tex("XIX - I")
+        roman.set_color(color)
+        roman.next_to(math, DOWN, MED_LARGE_BUFF)
+
+        sur_math1 = SurroundingRectangle(math[:2], stroke_color=color, stroke_width=3).round_corners(0.05)
+        sur_roman1 = SurroundingRectangle(roman[:3], stroke_color=color, stroke_width=3).round_corners(0.05)
+
+        self.play(
+            FadeIn(roman[:3], DOWN * 0.5),
+            ShowCreation(sur_math1),
+            roman[:3].animate.set_color(GOLD).set_anim_args(rate_func=there_and_back)
+        )
         self.wait()
