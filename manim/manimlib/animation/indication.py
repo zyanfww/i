d8b17e6639d6a4a3f9d5719fb3366f4cb25f407a
zyanfww <3blue1brownhindi@gmail.com>
2026-08-20 04:36:33 +0000

    f

--- manim/manimlib/animation/indication.py
+++ manim/manimlib/animation/indication.py
@@ -271,7 +271,7 @@ class FlashAround(VShowPassingFlash):
         path.insert_n_curves(n_inserted_curves)
         path.set_points(path.get_points_without_null_curves())
         path.set_stroke(color, stroke_width)
-        super().__init__(path, time_width=time_width,  taper_width=taper_width, **kwargs)
+        super().__init__(path, time_width=time_width, taper_width=taper_width, **kwargs)
 
     def get_path(self, mobject: Mobject, buff: float, round_corners: float | None) -> SurroundingRectangle:
         if round_corners is not None:
