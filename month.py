52fcf98fec21282a5c3a0e9ac6d15cb56b0940cd
zyanfww <3blue1brownhindi@gmail.com>
2026-08-21 04:07:08 +0000

    f

--- month.py
+++ month.py
@@ -3,8 +3,8 @@ from manimlib import *
 class YourProbelm(InteractiveScene):
     def construct(self):
         color = [PURPLE_A, PURPLE_C]
-        problem = Tex(R"\mathrm{Y}our \mathrm{B}ith \mathrm{M}onth")
-        math = Tex(R"\mathrm{Y}our \mathrm{M}ath \mathrm{P}roblem")
+        problem = TexText(R"$\mathbb{Y}$our $\mathbb{B}$irth $\mathbb{M}$onth")
+        math = TexText(R"$\mathbb{Y}$our $\mathbb{M}$ath $\mathbb{P}$roblem")
 
         problem.shift(UP * 0.5)
         math.next_to(problem, DOWN, buff=MED_LARGE_BUFF)
