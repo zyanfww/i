335f348c8022706686dc2a47c26e2a931f357d82
zyanfww <3blue1brownhindi@gmail.com>
2026-08-21 04:54:09 +0000

    f

--- month.py
+++ month.py
@@ -35,5 +35,5 @@ class January(InteractiveScene):
         solve = TexText("Solve for $x$")
         solve.set_width(FRAME_WIDTH - 5)
         solve.set_color(color)
-        solve.next_to(problem, DOWN, buff=LARGE_BUFF)
+        solve.next_to(problem, DOWN, buff=LARGE_BUFF*1.25)
         self.add(solve)
