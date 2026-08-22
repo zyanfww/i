ea59ac71d984c175a969f5bc3dd212c9e533b872
zyanfww <3blue1brownhindi@gmail.com>
2026-08-22 17:54:15 +0000

    f

--- try.py
+++ try.py
@@ -4,7 +4,7 @@ from manimlib import *
 class TestMatchingStrings(Scene):
     def construct(self):
         source = Tex("x + y + z")        
-        target = Tex("x - y - z")
+        target = Tex("x - y + z")
 
         #source.to_edge(LEFT)
         #target.to_edge(RIGHT)
