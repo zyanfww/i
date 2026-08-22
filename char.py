926b7f1c9f4a781dd0b66bb6f41b50d1baff7573
zyanfww <3blue1brownhindi@gmail.com>
2026-08-22 08:13:37 +0000

    f

--- char.py
+++ char.py
@@ -175,13 +175,15 @@ class F(InteractiveScene):
 
         problem = Tex(R"\sum_{n=1}^{\infty}\frac{(-1)^{n+1}}{n}")
         problem.set_color(color)
-        problem.shift(UP * 2)
+        problem.shift(UP)
+        problem.set_width(FRAME_WIDTH - 4)
+
         self.add(problem)
 
         solve = TexText("Find the sum")
         solve.set_color(color)
-        solve.set_width(3.8)
-        solve.next_to(problem, DOWN, buff=LARGE_BUFF)
+        solve.set_width(3)
+        solve.next_to(problem, DOWN, buff=LARGE_BUFF * 1.5)
         self.add(solve)
 
 
