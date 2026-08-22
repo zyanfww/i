99361b794fd378ca43187aaedb0ae40c74244a7d
zyanfww <3blue1brownhindi@gmail.com>
2026-08-22 18:27:24 +0000

    f

--- try.py
+++ try.py
@@ -3,5 +3,6 @@ from manimlib import *
 
 class TestMatchingStrings(Scene):
     def construct(self):
-        e = Tex(R"\frac{x}{y}")
+        e = Tex(R"\frac{x}{y}", isolate=["x", "y"])
+        e["x"].set_color(RED)
         self.add(e)
