36b770b8843249b4f6d3b6224b6dbe9bb057690c
zyanfww <3blue1brownhindi@gmail.com>
2026-08-22 17:25:22 +0000

    f

--- r
+++ r
@@ -1,3 +1,3 @@
-xvfb-run -a manimgl char.py -sw -r 2160x2160
+xvfb-run -a manimgl try.py -w -r 1920x1080
 # ffmpeg -i ./videos/Roman.mp4 -c copy -movflags +faststart -bsf:a aac_adtstoasc patch_identity.mp4
 ./g
---
+++ try.py
@@ -0,0 +1,34 @@
+from manimlib import *
+
+
+class TestMatchingStrings(Scene):
+    def construct(self):
+        source = Tex("x + x")
+        target = Tex("x^2 + x")
+
+        source.to_edge(LEFT)
+        target.to_edge(RIGHT)
+
+        self.add(source)
+        self.wait()
+
+        # Get the two x occurrences from source
+        source_x1 = source[0]
+        source_x2 = source[2]
+
+        # Get the two x occurrences from target
+        target_x1 = target[0]
+        target_x2 = target[3]
+
+        self.play(
+            TransformMatchingStrings(
+                source,
+                target,
+                matched_pairs=[
+                    (source_x1, target_x1),
+                    (source_x2, target_x2),
+                ],
+            )
+        )
+
+        self.wait()
