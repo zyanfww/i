6042acfa5666f50dfdb6b45e3f05e45510d1f98f
zyanfww <3blue1brownhindi@gmail.com>
2026-08-22 17:36:24 +0000

    f

--- try.py
+++ try.py
@@ -3,8 +3,7 @@ from manimlib import *
 
 class TestMatchingStrings(Scene):
     def construct(self):
-        source = Tex("x + x")
-        target = Tex("x^2 + x")
+        source = Tex("x + y = 2")        target = Tex("x = 2 - y")
 
         source.to_edge(LEFT)
         target.to_edge(RIGHT)
@@ -12,23 +11,11 @@ class TestMatchingStrings(Scene):
         self.add(source)
         self.wait()
 
-        # Get the two x occurrences from source
-        source_x1 = source[0]
-        source_x2 = source[2]
-
-        # Get the two x occurrences from target
-        target_x1 = target[0]
-        target_x2 = target[3]
-
         self.play(
             TransformMatchingStrings(
                 source,
                 target,
-                key_map={"x": 2},
-                matched_pairs=[
-                    (source_x1, target_x1),
-                    (source_x2, target_x2),
-                ],
+                key_map={"+": "-"},
             )
         )
 
