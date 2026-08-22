0f19a7d28723fb8804bd2ffb11172e781f41354c
zyanfww <3blue1brownhindi@gmail.com>
2026-08-22 17:39:08 +0000

    f

--- try.py
+++ try.py
@@ -6,8 +6,8 @@ class TestMatchingStrings(Scene):
         source = Tex("x + y = 2")        
         target = Tex("x = 2 - y")
 
-        source.to_edge(LEFT)
-        target.to_edge(RIGHT)
+        #source.to_edge(LEFT)
+        #target.to_edge(RIGHT)
 
         self.add(source)
         self.wait()
