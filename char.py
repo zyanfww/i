e54c3021a189edca238f7629c6b4f66d760006a7
zyanfww <3blue1brownhindi@gmail.com>
2026-08-23 05:00:02 +0000

    f

--- char.py
+++ char.py
@@ -407,13 +407,15 @@ class N(InteractiveScene):
 
         problem = Tex(R"\sum_{n=1}^{\infty}\frac{1}{n^s}")
         problem.set_color(color)
-        problem.shift(UP * 2)
+        problem.shift(UP)
+        problem.set_width(FRAME_WIDTH - 5)
+
         self.add(problem)
 
         solve = TexText("Identify the function")
         solve.set_color(color)
-        solve.set_width(4.8)
-        solve.next_to(problem, DOWN, buff=LARGE_BUFF)
+        solve.set_width(4)
+        solve.next_to(problem, DOWN, buff=LARGE_BUFF * 1.5)
         self.add(solve)
 
 
