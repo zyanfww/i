44e6cb73a5df22ff8b97258d646ca311cf64465d
zyanfww <3blue1brownhindi@gmail.com>
2026-08-29 07:39:49 +0000

    f

--- test.py
+++ test.py
@@ -243,10 +243,10 @@ class Dis(PaidTemplate):
         self.add(t, under)
         dis = TexText(
             R"""
-            This collection contains educational resources gathered and organized
-            for convenience. I do not claim ownership of any third-party copyrighted
-            content. All rights remain with their respective authors, publishers,
-            and copyright holders.
+            This collection contains educational \\resources gathered and organized
+            for convenience. I do not claim\\ ownership of any third-party copyrighted\\
+            content. All rights remain with\\ their respective authors, publishers,
+            and \\copyright holders.
             """, alignment="left"
         )
         dis.set_width(FRAME_WIDTH - 2)
