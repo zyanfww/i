d7fe4fdca418c2841312a825aca299eed91e81ed
zyanfww <3blue1brownhindi@gmail.com>
2026-08-30 03:27:08 +0000

    f

--- gtest.py
+++ gtest.py
@@ -4,5 +4,5 @@ class Test(InteractiveScene):
     def construct(self):
         s = Square()
         s.set_width(FRAME_WIDTH - 1)
-        s.set_fill(GREY, 1.0, gradient_direction=UR)
+        s.set_fill([GREY_A, GREY_D], 1.0, gradient_direction=UR)
         self.add(s)
